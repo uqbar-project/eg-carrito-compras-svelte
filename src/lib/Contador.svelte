b9fa8124d540264d3085f4f983506d62bf4d854e
Fernando Dodino <fernando.dodino@gmail.com>
2024-11-23 11:12:30 -0300

    Initial commit

---
+++ src/lib/Contador.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import './contador.css'
+
+  let { valorInicial = 0, desde = undefined, hasta = undefined } = $props()
+	let count = $state(valorInicial)
+
+	const restar = () => {
+    count--
+    count = !!desde && count < desde ? hasta : count
+  }
+	const sumar = () => {
+    count++
+    count = !!hasta && count > hasta ? desde : count
+  }
+</script>
+
+<div class='box'>
+  <button class='secondary' onclick={restar}>-</button>
+  <span>{count}</span>
+  <button class='primary' onclick={sumar}>+</button>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Contador from '$lib/Contador.svelte'
+</script>
+
+<Contador valorInicial={1} desde={1} hasta={4}></Contador>
+<Contador></Contador>
